18b3beff19f03d3cc909ed4dfcaefc71a0a14a54
Thanh Do <dothanh2572005@gmail.com>
2026-06-15 15:11:47 +0700

    first commit

---
+++ src/main/java/com/dotha/websocket/se2034jv_thanhdhhe194260_carrentingsystem/Se2034JvThanhDhhe194260CarRentingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.dotha.websocket.se2034jv_thanhdhhe194260_carrentingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Se2034JvThanhDhhe194260CarRentingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Se2034JvThanhDhhe194260CarRentingSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dotha/websocket/se2034jv_thanhdhhe194260_carrentingsystem/Se2034JvThanhDhhe194260CarRentingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dotha.websocket.se2034jv_thanhdhhe194260_carrentingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Se2034JvThanhDhhe194260CarRentingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
